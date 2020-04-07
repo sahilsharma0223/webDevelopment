1c408f57551625115963a94bba819ebbd624d37a
SAHIL SHARMA <shubhamsharma4965@gmail.com>
2020-04-07 22:03:52 +0530

    Add favicon and improvement in js for calculator

--- calculator/index.js
+++ calculator/index.js
@@ -4,39 +4,44 @@ var num1 = 0;
 var num2 = 0;
 var symbol =0;
 var result = 0;
+var error ='';
 var numPrint='';
   $("button").click (function() {
      var num = this.textContent;
      var sym = this.value;
 
-     if(sym==='/'||sym==='*'||sym==='+'||sym==='-'){
+     if(error!='Error!'&&-sym==='/'||sym==='*'||sym==='+'||sym==='-'){
        num1 = parseInt(numPrint);
-       console.log(num1);
        symbol = sym;
-       console.log(symbol);
        index = numPrint.length;
-       console.log(index);
      }
-     if(sym==='='){
-        num2 = parseInt(numPrint.substring(index+1,numPrint.length));
-        console.log(num2);
+
+     if(error!='Error!'&&sym==='='){
+       var symerror = numPrint.substring(index-1,index);
+       if(symerror==='×'||symerror==='÷'||symerror==='-'||symerror==='+'){
+         $(".entry").text("Error!");
+         $(".answer").text("Error!");
+         error = 'Error!';
+       }
+       else{
+       num2 = parseInt(numPrint.substring(index+1,numPrint.length));
         index = 0;
        equal(symbol);
+     }
      }
      if(num==='AC'){
        AC();
      }
-     else if(num==='CE'){
+     else if(error!='Error!'&&sym==='CE'){
        CE();
      }
-     else if(num!=='AC'&&num!=='CE'&&num!=='='){
+     else if(error!='Error!'&&num!=='AC'&&num!=='CE'&&num!=='='){
         entryPrint(num);
      }
 
   });
 
 function entryPrint(num){
-   console.log(numPrint+num);
    $(".entry").text(numPrint+num);
        numPrint = numPrint+num;
 }
@@ -45,11 +50,11 @@ function AC(){
   $(".entry").text("0");
   $(".answer").text("0");
   numPrint=[];
+  error = '';
 }
 
 function CE(){
   numPrint = numPrint.slice(0,-1);
-  console.log(numPrint.length);
   if(numPrint.length===0){
     $(".entry").text("0");
   }
@@ -71,10 +76,6 @@ function equal(symbol){
   else{
     result = num1/num2;
   }
-  console.log(num1);
-  console.log(num2);
-  console.log(result);
   $(".answer").text(result);
   numPrint=result.toString();
-  console.log(numPrint)
 }
